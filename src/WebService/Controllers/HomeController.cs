41db1d2f9ea01ec80ad06343ecda35b1452c90d5
Vaclav Turecek <vaclav@live.com>
2016-09-18 20:25:34 -0700

    Better path concatenation and application name on the main page.

--- src/WebService/Controllers/HomeController.cs
+++ src/WebService/Controllers/HomeController.cs
@@ -5,11 +5,20 @@
 namespace WebService.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using System.Fabric;
 
     public class HomeController : Controller
     {
+        private readonly StatelessServiceContext context;
+
+        public HomeController(StatelessServiceContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
+            ViewData["ApplicationName"] = this.context.CodePackageActivationContext.ApplicationName;
             return View();
         }
         
